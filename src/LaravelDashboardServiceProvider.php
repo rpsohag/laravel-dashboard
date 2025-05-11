99c49e2f640b8d0e623dd50f0c290b92e28452e9
rpsohag <rpsohag.bd@gmail.com>
2025-05-11 23:05:41 +0600

    initialize composer and create LaravelDashboardServiceProvider file

---
+++ src/LaravelDashboardServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rpsohag\LaravelDashboard;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDashboardServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
